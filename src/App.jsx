9a08e0fddc88fe950d5c6ac584f7eb0330517e8d
Luis Mariscal <luisferdev16@gmail.com>
2024-01-29 16:36:35 -0600

    adding useState to input range

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react"
+import Header from "./components/Header.jsx"
 function App() {
 
+  const [cantidad, setCantidad] = useState(10000);
+
+  function handleChange(e) {
+    console.log(+e.target.value);
+  }
+
   return (
-    <h1 className="text-6xl font-extrabold">Hello World From React+Vite</h1>
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <Header/>
+
+      <input
+       type='range'
+       className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+       onChange={handleChange}
+      />
+    </div>
   )
 }
 
