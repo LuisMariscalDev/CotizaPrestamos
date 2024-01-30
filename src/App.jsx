90ad3734b8cab2a8eaf3b6a0f03dc3963fc2dbae
Luis Mariscal <luisferdev16@gmail.com>
2024-01-30 17:02:45 -0600

    project finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Header from "./components/Header.jsx"
 import Button from "./components/Button.jsx";
-import { formatMoney } from "./helpers";
+import { formatMoney, calcularTotalPagar } from "./helpers";
 function App() {
 
   const [cantidad, setCantidad] = useState(10000);
   const [meses, setMeses] = useState(6)
+  const [total, setTotal] = useState(0);
+  const [pago, setPago] = useState(0);
+
+  useEffect(() => {
+    const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
+    setTotal(resultadoTotalPagar);
+  }, [cantidad, meses]);
+
+  useEffect(() => {
+    // Calcular el pago a meses
+    setPago(total / meses);
+  }, [total]);
 
   const MIN = 0;
   const MAX = 20000;
@@ -80,6 +92,16 @@ function App() {
         <option value="12">12 Meses</option>
         <option value="24">24 Meses</option>
       </select>
+
+      <div className="my-5 space-y-3 bg-gray-50 p-5">
+        <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+          Resumen <span className="text-indigo-600">de pagos</span>
+        </h2>
+
+        <p className="text-xl text-gray-500 text-center font-bold">{meses} Meses</p>
+        <p className="text-xl text-gray-500 text-center font-bold">{formatMoney(total)} Total a pagar</p>
+        <p className="text-xl text-gray-500 text-center font-bold">{formatMoney(pago)} Mensuales</p>
+      </div>
     </div>
   )
 }
